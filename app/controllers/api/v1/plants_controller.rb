9c542d719d9b1faa7722d7aa7cebe01872641f36
juliafealves <giT#8985>
2019-12-03 19:09:39 -0300

    Bug in heroku

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -1,21 +1,17 @@
-module Api
-  module V1
-    class PlantsController < ApplicationController
+class Api::V1::PlantsController < ApplicationController
 
-      # Created a plant.
-      def create
-        plant = Plant.new(plants_params)
-        if plant.save
-          render json: {status: 'SUCCESS', message: 'Saved plant', data: plant}, status: :created
-        else
-          render json: {status: 'ERROR', message: 'Plant not saved', data: plant.errors}, status: :unprocessable_entity
-        end
-      end
-
-      # Parameters accepts
-      def plants_params
-        params.permit(:name, :description)
-      end
+  # Created a plant.
+  def create
+    plant = Plant.new(plants_params)
+    if plant.save
+      render json: {status: 'SUCCESS', message: 'Saved plant', data: plant}, status: :created
+    else
+      render json: {status: 'ERROR', message: 'Plant not saved', data: plant.errors}, status: :unprocessable_entity
     end
+  end
+
+  # Parameters accepts
+  def plants_params
+    params.permit(:name, :description)
   end
 end
