9f1d6e2a8a74771a7922c4f8a7ef00e33dd6c270
juliafealves <giT#8985>
2019-12-03 17:40:31 -0300

    Added plant registration service.

---
+++ app/controllers/api/v1/plants_controller.rb
@@ -0,0 +1,21 @@
+module Api
+  module V1
+    class PlantsController < ApplicationController
+
+      # Created a plant.
+      def create
+        plant = Plant.new(plants_params)
+        if plant.save
+          render json: {status: 'SUCCESS', message: 'Saved plant', data: plant}, status: :created
+        else
+          render json: {status: 'ERROR', message: 'Plant not saved', data: plant.errors}, status: :unprocessable_entity
+        end
+      end
+
+      # Parameters accepts
+      def plants_params
+        params.permit(:name, :description)
+      end
+    end
+  end
+end
