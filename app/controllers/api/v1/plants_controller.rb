481db4b190135e2df46074fe898b860455681815
juliafealves <giT#8985>
2019-12-06 19:12:40 -0300

    Updated a plant.

--- app/controllers/api/v1/plants_controller.rb
+++ app/controllers/api/v1/plants_controller.rb
@@ -3,10 +3,22 @@ class Api::V1::PlantsController < ApplicationController
   # Created a plant.
   def create
     plant = Plant.new(plants_params)
+
     if plant.save
       render json: {status: 'SUCCESS', message: 'Saved plant', data: plant}, status: :created
     else
       render json: {status: 'ERROR', message: 'Plant not saved', data: plant.errors}, status: :unprocessable_entity
+    end
+  end
+
+  # Updated a plant.
+  def update
+    plant = Plant.find(params[:id])
+
+    if plant.update_attributes(plants_params)
+      render json: {status: 'SUCCESS', message: 'Updated plant', data: plant}, status: :ok
+    else
+      render json: {status: 'ERROR', message: 'Plant not updated', data: plant.errors}, status: :unprocessable_entity
     end
   end
 
